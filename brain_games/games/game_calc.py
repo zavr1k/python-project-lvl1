8b1efde6671ec3374490b380d7839724fcd3ea02
Anton <zavr35rus@gmail.com>
2020-10-21 23:17:47 +0300

    Changed game-calc

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -1,32 +1,20 @@
-from random import randrange, choice
 import prompt
-
-
-def _sum(a, b):
-    return a + b
-
-
-def _sub(a, b):
-    return a - b
-
-
-def _mul(a, b):
-    return a * b
+from random import randrange, choice
+from operator import sub, add, mul
 
 
 def calc():
-    operators = ('+', '-', '*')
-    operators_library = {
-        '+': _sum,
-        '-': _sub,
-        '*': _mul,
+    operators = {
+        '+': add,
+        '-': sub,
+        '*': mul,
     }
     number1 = randrange(99)
     number2 = randrange(99)
-    operator = choice(operators)
+    operator = choice(list(operators.keys()))
     print(f"Question: {number1} {operator} {number2}")
     answer = prompt.integer("Your answer: ")
-    result = operators_library[operator](number1, number2)
+    result = operators[operator](number1, number2)
     if answer == int(result):
         return True, "Correct!"
     return False, f"{answer} is wrong answer ;(. Correct answer was {result}"
