39a2fb2324443d960e392be773c3b1ee713b69b9
Anton <zavr35rus@gmail.com>
2020-11-09 21:14:25 +0300

    refactored gcd function

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,23 +1,13 @@
 from random import randrange
+from math import gcd
 
 
 RULES = "Find the greatest common divisor of given numbers."
-
-
-def get_gcd(*args):
-    numbers = list(args)  # puts the inout arguments into a list
-    n = max(numbers)
-
-    # generates a descending list from the biggest input number down to 1
-    for i in range(n, 0, -1):
-        # if all the (input numbers mod i) divide equally
-        if all(num % i == 0 for num in numbers):
-            return i
 
 
 def start_round():
     number1 = randrange(199)
     number2 = randrange(199)
     question = f"{number1} {number2}"
-    answer = str(get_gcd(number1, number2))
+    answer = str(gcd(number1, number2))
     return question, answer
