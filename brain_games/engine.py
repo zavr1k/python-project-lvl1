bfe6725af9e7b1dd84142be193578cf130d4d725
Anton <zavr35rus@gmail.com>
2020-11-12 11:17:37 +0300

    Moved raounds_total to finction argument

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,14 +1,13 @@
 import prompt
 
 
-def run_game(game=None):
+def run_game(game=None, rounds_total=3):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'(Hello, {name}!)')
     if game:
         print(game.DESCRIPTION)
-        NUMBER_OF_ROUNDS = 3
-        for _ in range(NUMBER_OF_ROUNDS):
+        for _ in range(rounds_total):
             question, answer = game.prepare_round()
             print(f"Question: {question}")
             user_answer = prompt.string("Your answer: ")
